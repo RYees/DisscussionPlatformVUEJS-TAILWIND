44e73b0544e806f6131cbcb47d846bf91feb598a
rehmet yeshanew <rehmetyeshanew@gmail.com>
2021-05-12 09:39:56 +0300

    updated

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
-// const history = require('connect-history-api-fallback');
+const history = require('connect-history-api-fallback');
 
 const app = express()
 
-app.use('/', serveStatic(path.join(__dirname,'/dist')))
+app.use('/', serveStatic(path.join(__dirname,'/dist')));
+app.use(history()); 
 // app.use(history({
 //     disableDotRule: true,
 //     verbose: true
 //   }));
 
 const port = process.env.PORT || 8080
-app.listen(port)
+app.listen(port);
 
 console.log('Listening on port:' + port)
